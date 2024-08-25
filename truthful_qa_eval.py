f2227548def01e68c686b480fb00408641ab8e43
sert121 <yashmore@YASHs-MacBook-Air.local>
2024-08-25 16:16:21 -0400

    added truthfulqa

--- truthful_qa_eval.py
+++ truthful_qa_eval.py
@@ -7,6 +7,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import transformers
 import torch
 import vllm
+from huggingface_hub import snapshot_download
+
 
 # Define benchmark with specific tasks and number of code generations
 benchmark = MMLU(
@@ -14,23 +16,27 @@ benchmark = MMLU(
 )
 
 class CustomLM(DeepEvalBaseLLM):
-    def __init__(self, model_name="meta-llama/Meta-Llama-3-8B-Instruct"):
-        quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_compute_dtype=torch.float16,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
-        )
+    def __init__(self, model_name="meta-llama/Meta-Llama-3-8B-Instruct", lora_path=None):
 
         # load quantization config from json if any !TODO
+        # just take the quantized model path (assume the model is already quantized)
 
+
+        self.model_name = model_name
         # load the sampling params
         self.sampling_params = SamplingParams(temperature=0)
 
         # load lora config from json if any !TODO
+        # if the lora adapters are present, take the path
+        if lora_path is not None:
+            adapter_lora_path = snapshot_download(repo_id=sql_lora_path)
+            llm = vllm.LLM(model=model_name, enable_lora=True)
+            self.lora_enabled = True
+            self.lora_request = LoRARequest("lora_adapter", 1, lora)
 
-        # insert vllm code to load model (with above c)
-        llm = vllm.LLM(model=model_name)
+        else:
+            # insert vllm code to load model (with above c)
+            llm = vllm.LLM(model=model_name)
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         self.model = llm
@@ -43,8 +49,17 @@ class CustomLM(DeepEvalBaseLLM):
         # generate a single prompt output
         model = self.load_model()
 
-        output = model.generate(prompt, self.sampling_params)
-        # generate the model outputs using vllm
+
+        # generate the model output
+        if self.lora_enabled:
+            output = model.generate(prompt, 
+            self.sampling_params, 
+            self.lora_request)
+        else:
+            output = model.generate(prompt, self.sampling_params)
+
+
+        # extract the model outputs
         generated_text = output.outputs[0].text
         # return the output
         return generated_text
@@ -53,7 +68,7 @@ class CustomLM(DeepEvalBaseLLM):
         return self.generate(prompt)
 
     def get_model_name(self):
-        return "model name "
+        return f"model name {self.model_name}"
 
 benchmark.evaluate(model = CustomLM()) # no parameter k 
 # benchmark.evaluate(model=gpt_4, k=1)
